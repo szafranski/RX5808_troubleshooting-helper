8af8c8aef7428fa58b0da8cb849f51d6cf9b30af
szafranski <23621888+szafranski@users.noreply.github.com>
2020-02-10 08:08:38 +0100

    Update rx5808_trs_helper.ino

--- rx5808_trs_helper.ino
+++ rx5808_trs_helper.ino
@@ -1,9 +1,10 @@
-// this code makes Arduino built-in led blinking fast when it detects stron vtx signal near the rx5808 receiver
+// this code makes Arduino built-in led blinking fast when it detects strong vtx signal near the rx5808 receiver
 
 // vtx transmitter should be tune to 5885MHz (E5 channel) +/- 5Mhz (5880MHz - R7 is also working)
 
 // if you want to use another channel - you can tune the receiver accordingly to this table: 
 //  https://oscarliang.com/ctt/uploads/2016/02/vtx-frequency-5-band-40-channel-boscam-fatshark-raceband-abef.png
+
 /*
 
  
