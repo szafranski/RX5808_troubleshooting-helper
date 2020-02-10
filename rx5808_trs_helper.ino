598499d3d033712d1f4c9eb5abfd579aebd708bf
szafranski <23621888+szafranski@users.noreply.github.com>
2020-02-10 08:07:21 +0100

    Create rx5808_trs_helper.ino

---
+++ rx5808_trs_helper.ino
@@ -0,0 +1,57 @@
+// this code makes Arduino built-in led blinking fast when it detects stron vtx signal near the rx5808 receiver
+
+// vtx transmitter should be tune to 5885MHz (E5 channel) +/- 5Mhz (5880MHz - R7 is also working)
+
+// if you want to use another channel - you can tune the receiver accordingly to this table: 
+//  https://oscarliang.com/ctt/uploads/2016/02/vtx-frequency-5-band-40-channel-boscam-fatshark-raceband-abef.png
+/*
+
+ 
+connect the RX5808 to the Arduino:
+
+RX5808   Arduino
+  CH1      11
+  CH2      10
+  CH3      13
+  RSSI     A0
+  GND      GND
+   5V      5V
+
+
+
+*/
+
+#define CH1 11
+#define CH2 10
+#define CH3 13
+#define RSSIpin A0
+#define ledPin 13
+
+int rssi_readout;
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(RSSIpin, INPUT);
+pinMode(CH1, OUTPUT);
+pinMode(CH2, OUTPUT);
+pinMode(CH3, OUTPUT);
+pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+rssi_readout = analogRead(RSSIpin);
+
+digitalWrite(CH1, LOW); // sets reciver frequency to 5885MHz - can be changed as mentioned in the comment at the top
+digitalWrite(CH2, LOW);
+digitalWrite(CH3, HIGH);
+
+if (rssi_readout >750)          // makes built-in led blinking when quad is near the receiver 
+{ digitalWrite(ledPin, HIGH);
+delay(300);
+digitalWrite(ledPin, LOW);
+delay(300);
+}
+else digitalWrite(ledPin, LOW);
+
+}
